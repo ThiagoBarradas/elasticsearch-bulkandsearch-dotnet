0d6f17a85a921dfa39ba3bb6839dc4dcd9132ec0
Bernardo Esbérard <bernardbr@gmail.com>
2020-08-05 17:53:42 -0300

    :white_check_mark: Implements tests for async methods.

--- Elasticsearch.BulkAndSearch.Tests/ElasticsearchCommandTests.cs
+++ Elasticsearch.BulkAndSearch.Tests/ElasticsearchCommandTests.cs
@@ -1,6 +1,7 @@
 ﻿using Elasticsearch.BulkAndSearch.Tests.TestUtilities;
 using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace Elasticsearch.BulkAndSearch.Tests
@@ -28,6 +29,26 @@ namespace Elasticsearch.BulkAndSearch.Tests
         }
 
         [Fact]
+        public static async Task UpsertAsync_Should_Index_In_Default_Index()
+        {
+            // arrage
+            var options = ElasticClientMock.GetOptions();
+            var command = new ElasticsearchCommand<Person>(options, null);
+            command.ElasticClient = ElasticClientMock.GetElasticClientMock(options, null, null, null);
+            var person = new Person { Id = "2", Name = "Bernardo Esbérard", Age = 36, CreateDate = new DateTime(2020, 03, 26) };
+
+            // act
+            var result = await command.UpsertAsync(person);
+
+            // assert
+            Assert.True(result);
+            Assert.Equal("IndexAsync", ElasticClientMock.LastElasticClientAction);
+            Assert.Equal(options.DefaultIndexName, ElasticClientMock.LastProcessedIndex);
+            Assert.Equal(person.Id, ElasticClientMock.LastProcessedPerson.Id);
+            Assert.Equal(person.Name, ElasticClientMock.LastProcessedPerson.Name);
+        }
+
+        [Fact]
         public static void Upsert_Should_Index_In_Default_Index_With_Period()
         {
             // arrage
@@ -48,6 +69,26 @@ namespace Elasticsearch.BulkAndSearch.Tests
         }
 
         [Fact]
+        public static async Task UpsertAsync_Should_Index_In_Default_Index_With_Period()
+        {
+            // arrage
+            var options = ElasticClientMock.GetOptions();
+            var command = new ElasticsearchCommand<Person>(options, Person.GenerateIndexName);
+            command.ElasticClient = ElasticClientMock.GetElasticClientMock(options, Person.GenerateIndexName, null, null);
+            var person = new Person { Id = "2", Name = "Bernardo Esbérard", Age = 36, CreateDate = new DateTime(2020, 03, 26) };
+
+            // act
+            var result = await command.UpsertAsync(person);
+
+            // assert
+            Assert.True(result);
+            Assert.Equal("IndexAsync", ElasticClientMock.LastElasticClientAction);
+            Assert.Equal(options.DefaultIndexName + "-2020-03", ElasticClientMock.LastProcessedIndex);
+            Assert.Equal(person.Id, ElasticClientMock.LastProcessedPerson.Id);
+            Assert.Equal(person.Name, ElasticClientMock.LastProcessedPerson.Name);
+        }
+
+        [Fact]
         public static void Bulk_Should_Index_In_Default_Index()
         {
             // arrage
@@ -56,8 +97,8 @@ namespace Elasticsearch.BulkAndSearch.Tests
             command.ElasticClient = ElasticClientMock.GetElasticClientMock(options, null, null, null);
             var persons = new List<Person>
             {
-                { new Person { Id = "1", Name = "Thiago Barradas", Age = 27, CreateDate = new DateTime(2019, 01, 01) } },
-                { new Person { Id = "2", Name = "Raphael Barradas", Age = 29, CreateDate = new DateTime(2018, 12, 01) } }
+                new Person { Id = "1", Name = "Thiago Barradas", Age = 27, CreateDate = new DateTime(2019, 01, 01) },
+                new Person { Id = "2", Name = "Raphael Barradas", Age = 29, CreateDate = new DateTime(2018, 12, 01) }
             };
 
             // act
@@ -66,6 +107,33 @@ namespace Elasticsearch.BulkAndSearch.Tests
             // assert
             Assert.True(result.IsValid);
             Assert.Equal("Bulk", ElasticClientMock.LastElasticClientAction);
+            Assert.Equal(options.DefaultIndexName, ElasticClientMock.LastProcessedIndexes[0]);
+            Assert.Equal(options.DefaultIndexName, ElasticClientMock.LastProcessedIndexes[1]);
+            Assert.Equal(persons[0].Id, ElasticClientMock.LastProcessedPersons[0].Id);
+            Assert.Equal(persons[0].Name, ElasticClientMock.LastProcessedPersons[0].Name);
+            Assert.Equal(persons[1].Id, ElasticClientMock.LastProcessedPersons[1].Id);
+            Assert.Equal(persons[1].Name, ElasticClientMock.LastProcessedPersons[1].Name);
+        }
+
+        [Fact]
+        public static async Task BulkAsync_Should_Index_In_Default_Index()
+        {
+            // arrage
+            var options = ElasticClientMock.GetOptions();
+            var command = new ElasticsearchCommand<Person>(options, null);
+            command.ElasticClient = ElasticClientMock.GetElasticClientMock(options, null, null, null);
+            var persons = new List<Person>
+            {
+                new Person { Id = "3", Name = "Bernardo Esbérard", Age = 36, CreateDate = new DateTime(2020, 03, 26) },
+                new Person { Id = "4", Name = "Mariana Esbérard", Age = 33, CreateDate = new DateTime(2019, 08, 07) }
+            };
+
+            // act
+            var result = await command.BulkAsync(persons);
+
+            // assert
+            Assert.True(result.IsValid);
+            Assert.Equal("BulkAsync", ElasticClientMock.LastElasticClientAction);
             Assert.Equal(options.DefaultIndexName, ElasticClientMock.LastProcessedIndexes[0]);
             Assert.Equal(options.DefaultIndexName, ElasticClientMock.LastProcessedIndexes[1]);
             Assert.Equal(persons[0].Id, ElasticClientMock.LastProcessedPersons[0].Id);
@@ -84,8 +152,8 @@ namespace Elasticsearch.BulkAndSearch.Tests
 
             var persons = new List<Person>
             {
-                { new Person { Id = "1", Name = "Thiago Barradas", Age = 27, CreateDate = new DateTime(2019, 01, 01) } },
-                { new Person { Id = "2", Name = "Raphael Barradas", Age = 29, CreateDate = new DateTime(2018, 12, 01) } }
+                new Person { Id = "1", Name = "Thiago Barradas", Age = 27, CreateDate = new DateTime(2019, 01, 01) },
+                new Person { Id = "2", Name = "Raphael Barradas", Age = 29, CreateDate = new DateTime(2018, 12, 01) }
             };
 
             // act
@@ -94,8 +162,36 @@ namespace Elasticsearch.BulkAndSearch.Tests
             // assert
             Assert.True(result.IsValid);
             Assert.Equal("Bulk", ElasticClientMock.LastElasticClientAction);
-            Assert.Equal(options.DefaultIndexName+"-2019-01", ElasticClientMock.LastProcessedIndexes[0]);
-            Assert.Equal(options.DefaultIndexName+"-2018-12", ElasticClientMock.LastProcessedIndexes[1]);
+            Assert.Equal(options.DefaultIndexName + "-2019-01", ElasticClientMock.LastProcessedIndexes[0]);
+            Assert.Equal(options.DefaultIndexName + "-2018-12", ElasticClientMock.LastProcessedIndexes[1]);
+            Assert.Equal(persons[0].Id, ElasticClientMock.LastProcessedPersons[0].Id);
+            Assert.Equal(persons[0].Name, ElasticClientMock.LastProcessedPersons[0].Name);
+            Assert.Equal(persons[1].Id, ElasticClientMock.LastProcessedPersons[1].Id);
+            Assert.Equal(persons[1].Name, ElasticClientMock.LastProcessedPersons[1].Name);
+        }
+
+        [Fact]
+        public static async Task BulkAsync_Should_Index_In_Default_Index_With_Period()
+        {
+            // arrage
+            var options = ElasticClientMock.GetOptions();
+            var command = new ElasticsearchCommand<Person>(options, Person.GenerateIndexName);
+            command.ElasticClient = ElasticClientMock.GetElasticClientMock(options, Person.GenerateIndexName, null, null);
+
+            var persons = new List<Person>
+            {
+                new Person { Id = "3", Name = "Bernardo Esbérard", Age = 36, CreateDate = new DateTime(2020, 03, 26) },
+                new Person { Id = "4", Name = "Mariana Esbérard", Age = 33, CreateDate = new DateTime(2019, 08, 07) }
+            };
+
+            // act
+            var result = await command.BulkAsync(persons);
+
+            // assert
+            Assert.True(result.IsValid);
+            Assert.Equal("BulkAsync", ElasticClientMock.LastElasticClientAction);
+            Assert.Equal(options.DefaultIndexName + "-2020-03", ElasticClientMock.LastProcessedIndexes[0]);
+            Assert.Equal(options.DefaultIndexName + "-2019-08", ElasticClientMock.LastProcessedIndexes[1]);
             Assert.Equal(persons[0].Id, ElasticClientMock.LastProcessedPersons[0].Id);
             Assert.Equal(persons[0].Name, ElasticClientMock.LastProcessedPersons[0].Name);
             Assert.Equal(persons[1].Id, ElasticClientMock.LastProcessedPersons[1].Id);
--- Elasticsearch.BulkAndSearch.Tests/ElasticsearchQueryTests.cs
+++ Elasticsearch.BulkAndSearch.Tests/ElasticsearchQueryTests.cs
@@ -3,6 +3,7 @@ using Elasticsearch.BulkAndSearch.Tests.TestUtilities;
 using Nest;
 using System;
 using System.Linq;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace Elasticsearch.BulkAndSearch.Tests
@@ -26,6 +27,22 @@ namespace Elasticsearch.BulkAndSearch.Tests
         }
 
         [Fact]
+        public static async Task GetAsync_Should_Return_Zero_Documents()
+        {
+            // arrage
+            var options = ElasticClientMock.GetOptions();
+            var query = new ElasticsearchQuery<Person>(options);
+            query.ElasticClient = ElasticClientMock.GetElasticClientMock(options, null, null, null);
+
+            // act
+            var result = await query.GetAsync("some-id");
+
+            // assert
+            Assert.Null(result);
+            Assert.Equal("GetAsync", ElasticClientMock.LastElasticClientAction);
+        }
+
+        [Fact]
         public static void Get_Should_Return_Document()
         {
             // arrage
@@ -42,6 +59,25 @@ namespace Elasticsearch.BulkAndSearch.Tests
             Assert.Equal("1", result.Id);
             Assert.Equal("Thiago Barradas", result.Name);
             Assert.Equal("Get", ElasticClientMock.LastElasticClientAction);
+        }
+
+        [Fact]
+        public static async Task GetAsync_Should_Return_Document()
+        {
+            // arrage
+            var options = ElasticClientMock.GetOptions();
+            var query = new ElasticsearchQuery<Person>(options);
+            var person = new Person { Id = "2", Name = "Bernardo Esbérard", Age = 36, CreateDate = new DateTime(2020, 03, 26) };
+            query.ElasticClient = ElasticClientMock.GetElasticClientMock(options, null, person, null);
+
+            // act
+            var result = await query.GetAsync("some-id");
+
+            // assert
+            Assert.NotNull(result);
+            Assert.Equal("2", result.Id);
+            Assert.Equal("Bernardo Esbérard", result.Name);
+            Assert.Equal("GetAsync", ElasticClientMock.LastElasticClientAction);
         }
 
         [Fact]
@@ -62,6 +98,28 @@ namespace Elasticsearch.BulkAndSearch.Tests
             Assert.Empty(result.Items);
             Assert.Equal(0, result.Total);
             Assert.Equal("{\"from\":0,\"query\":{\"match\":{\"name\":{\"query\":\"Ralph Barradas\"}}},\"size\":1}", ElasticClientMock.LastQueryBody);
+            Assert.Equal("Search", ElasticClientMock.LastElasticClientAction);
+        }
+
+        [Fact]
+        public static async Task SearchAsync_Should_Return_Zero_Document()
+        {
+            // arrage
+            var options = ElasticClientMock.GetOptions();
+            var query = new ElasticsearchQuery<Person>(options);
+            var searchOptions = new SearchOptions { Page = 1, Size = 1 };
+            var queryToFilter = Query<Person>.Match(i => i.Field("name").Query("Berna dos Anjos"));
+            query.ElasticClient = ElasticClientMock.GetElasticClientMock(options, null, null, null);
+
+            // act
+            var result = await query.SearchAsync(queryToFilter, searchOptions);
+
+            // assert
+            Assert.NotNull(result);
+            Assert.Empty(result.Items);
+            Assert.Equal(0, result.Total);
+            Assert.Equal("{\"from\":0,\"query\":{\"match\":{\"name\":{\"query\":\"Berna dos Anjos\"}}},\"size\":1}", ElasticClientMock.LastQueryBody);
+            Assert.Equal("SearchAsync", ElasticClientMock.LastElasticClientAction);
         }
 
         [Fact]
@@ -74,7 +132,7 @@ namespace Elasticsearch.BulkAndSearch.Tests
             var searchOptions = new SearchOptions { Page = 1, Size = 1 };
             var queryToFilter = Query<Person>.Match(i => i.Field("name").Query("Thiago Barradas"));
             query.ElasticClient = ElasticClientMock.GetElasticClientMock(options, null, person, null);
-            
+
             // act
             var result = query.Search(queryToFilter, searchOptions);
 
@@ -86,6 +144,30 @@ namespace Elasticsearch.BulkAndSearch.Tests
             Assert.Equal("Thiago Barradas", result.Items.First().Name);
             Assert.Equal("Search", ElasticClientMock.LastElasticClientAction);
             Assert.Equal("{\"from\":0,\"query\":{\"match\":{\"name\":{\"query\":\"Thiago Barradas\"}}},\"size\":1}", ElasticClientMock.LastQueryBody);
+        }
+
+        [Fact]
+        public static async Task SearchAsync_Should_Return_Document()
+        {
+            // arrage
+            var options = ElasticClientMock.GetOptions();
+            var query = new ElasticsearchQuery<Person>(options);
+            var person = new Person { Id = "2", Name = "Bernardo Esbérard", Age = 27, CreateDate = new DateTime(2019, 01, 01) };
+            var searchOptions = new SearchOptions { Page = 1, Size = 1 };
+            var queryToFilter = Query<Person>.Match(i => i.Field("name").Query("Bernardo Esbérard"));
+            query.ElasticClient = ElasticClientMock.GetElasticClientMock(options, null, person, null);
+
+            // act
+            var result = await query.SearchAsync(queryToFilter, searchOptions);
+
+            // assert
+            Assert.NotNull(result);
+            Assert.Single(result.Items);
+            Assert.Equal(1, result.Total);
+            Assert.Equal("2", result.Items.First().Id);
+            Assert.Equal("Bernardo Esbérard", result.Items.First().Name);
+            Assert.Equal("SearchAsync", ElasticClientMock.LastElasticClientAction);
+            Assert.Equal("{\"from\":0,\"query\":{\"match\":{\"name\":{\"query\":\"Bernardo Esbérard\"}}},\"size\":1}", ElasticClientMock.LastQueryBody);
         }
 
         [Fact]
@@ -106,7 +188,32 @@ namespace Elasticsearch.BulkAndSearch.Tests
             Assert.Null(result.ScrollId);
             Assert.Empty(result.Items);
             Assert.Equal(0, result.Total);
+            // The expected value is 'Search' because it will search from start.
+            Assert.Equal("Search", ElasticClientMock.LastElasticClientAction); 
             Assert.Equal("{\"query\":{\"match\":{\"name\":{\"query\":\"Ralph Barradas\"}}},\"size\":1}", ElasticClientMock.LastQueryBody);
+        }
+
+        [Fact]
+        public static async Task ScrollAsync_Should_Return_Zero_Document()
+        {
+            // arrage
+            var options = ElasticClientMock.GetOptions();
+            var query = new ElasticsearchQuery<Person>(options);
+            var scrollOptions = new ScrollOptions { Scroll = "10m", Size = 1 };
+            var queryToFilter = Query<Person>.Match(i => i.Field("name").Query("Berna dos Anjos"));
+            query.ElasticClient = ElasticClientMock.GetElasticClientMock(options, null, null, null);
+
+            // act
+            var result = await query.ScrollAsync(queryToFilter, scrollOptions);
+
+            // assert
+            Assert.NotNull(result);
+            Assert.Null(result.ScrollId);
+            Assert.Empty(result.Items);
+            Assert.Equal(0, result.Total);
+            // The expected value is 'SearchAsync' because it will search from start.
+            Assert.Equal("SearchAsync", ElasticClientMock.LastElasticClientAction);
+            Assert.Equal("{\"query\":{\"match\":{\"name\":{\"query\":\"Berna dos Anjos\"}}},\"size\":1}", ElasticClientMock.LastQueryBody);
         }
 
         [Fact]
@@ -130,8 +237,35 @@ namespace Elasticsearch.BulkAndSearch.Tests
             Assert.Equal("1234567", result.ScrollId);
             Assert.Equal("1", result.Items.First().Id);
             Assert.Equal("Thiago Barradas", result.Items.First().Name);
+            // The expected value is 'Search' because it will search from start.
             Assert.Equal("Search", ElasticClientMock.LastElasticClientAction);
             Assert.Equal("{\"query\":{\"match\":{\"name\":{\"query\":\"Thiago Barradas\"}}},\"size\":1}", ElasticClientMock.LastQueryBody);
+        }
+
+        [Fact]
+        public static async Task ScrollAsync_Should_Return_Document()
+        {
+            // arrage
+            var options = ElasticClientMock.GetOptions();
+            var query = new ElasticsearchQuery<Person>(options);
+            var person = new Person { Id = "2", Name = "Bernardo Esbérard", Age = 36, CreateDate = new DateTime(2020, 03, 26) };
+            var scrollOptions = new ScrollOptions { Scroll = "10m", Size = 1 };
+            var queryToFilter = Query<Person>.Match(i => i.Field("name").Query("Bernardo Esbérard"));
+            query.ElasticClient = ElasticClientMock.GetElasticClientMock(options, null, person, "1234567");
+
+            // act
+            var result = await query.ScrollAsync(queryToFilter, scrollOptions);
+
+            // assert
+            Assert.NotNull(result);
+            Assert.Single(result.Items);
+            Assert.Equal(1, result.Total);
+            Assert.Equal("1234567", result.ScrollId);
+            Assert.Equal("2", result.Items.First().Id);
+            Assert.Equal("Bernardo Esbérard", result.Items.First().Name);
+            // The expected value is 'Search' because it will search from start.
+            Assert.Equal("SearchAsync", ElasticClientMock.LastElasticClientAction);
+            Assert.Equal("{\"query\":{\"match\":{\"name\":{\"query\":\"Bernardo Esbérard\"}}},\"size\":1}", ElasticClientMock.LastQueryBody);
         }
 
         [Fact]
@@ -156,6 +290,31 @@ namespace Elasticsearch.BulkAndSearch.Tests
             Assert.Equal("1", result.Items.First().Id);
             Assert.Equal("Thiago Barradas", result.Items.First().Name);
             Assert.Equal("Scroll", ElasticClientMock.LastElasticClientAction);
+            Assert.Equal("{\"scroll\":\"10m\",\"scroll_id\":\"1234567\"}", ElasticClientMock.LastQueryBody);
+        }
+
+        [Fact]
+        public static async Task ScrollAsync_Should_Return_Document_With_ScrollId()
+        { 
+            // arrage
+            var options = ElasticClientMock.GetOptions();
+            var query = new ElasticsearchQuery<Person>(options);
+            var person = new Person { Id = "2", Name = "Bernardo Esbérard", Age = 36, CreateDate = new DateTime(2020, 03, 26) };
+            var scrollOptions = new ScrollOptions { Scroll = "10m", ScrollId = "1234567", Size = 1 };
+            var queryToFilter = Query<Person>.Match(i => i.Field("name").Query("Bernardo Esbérard"));
+            query.ElasticClient = ElasticClientMock.GetElasticClientMock(options, null, person, "1234567");
+
+            // act
+            var result = await query.ScrollAsync(queryToFilter, scrollOptions);
+
+            // assert
+            Assert.NotNull(result);
+            Assert.Single(result.Items);
+            Assert.Equal(1, result.Total);
+            Assert.Equal("1234567", result.ScrollId);
+            Assert.Equal("2", result.Items.First().Id);
+            Assert.Equal("Bernardo Esbérard", result.Items.First().Name);
+            Assert.Equal("ScrollAsync", ElasticClientMock.LastElasticClientAction);
             Assert.Equal("{\"scroll\":\"10m\",\"scroll_id\":\"1234567\"}", ElasticClientMock.LastQueryBody);
         }
     }
