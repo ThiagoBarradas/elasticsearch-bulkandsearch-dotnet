d246bfe39e9ffe66c1c39cde63c3e4a063d1c653
Thiago Barradas <thiagobrowskt@gmail.com>
2021-09-09 12:49:34 -0300

    :fire: remove packutils

---
+++ Elasticsearch.BulkAndSearch/Converters/EnumWithContractJsonConverter.cs
@@ -0,0 +1,105 @@
+﻿using Elasticsearch.BulkAndSearch.Helpers;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Linq;
+using System.Reflection;
+using static Elasticsearch.BulkAndSearch.Helpers.JsonHelper;
+
+namespace Elasticsearch.BulkAndSearch.Converters
+{
+    public class EnumWithContractJsonConverter : JsonConverter
+    {
+        public static bool IgnoreEnumCase { get; set; } = false;
+
+        public override bool CanConvert(Type objectType)
+        {
+            Type type = IsNullableType(objectType)
+                ? Nullable.GetUnderlyingType(objectType)
+                : objectType;
+
+            return type.GetTypeInfo().IsEnum;
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            bool isNullable = IsNullableType(objectType);
+            Type enumType = isNullable
+                ? Nullable.GetUnderlyingType(objectType)
+                : objectType;
+
+            string[] names = Enum.GetNames(enumType);
+
+            if (reader.TokenType == JsonToken.String)
+            {
+                string enumText = reader.Value.ToString().ToLowerCase();
+
+                if (!string.IsNullOrEmpty(enumText))
+                {
+                    string match = names
+                        .Where(n => string.Equals(n.ToLowerCase(), enumText, StringComparison.OrdinalIgnoreCase))
+                        .FirstOrDefault();
+
+                    if (match != null)
+                    {
+                        return Enum.Parse(enumType, match);
+                    }
+                }
+            }
+            else if (reader.TokenType == JsonToken.Integer)
+            {
+                int enumVal = Convert.ToInt32(reader.Value);
+                int[] values = (int[])Enum.GetValues(enumType);
+                if (values.Contains(enumVal))
+                {
+                    return Enum.Parse(enumType, enumVal.ToString());
+                }
+            }
+
+            if (!isNullable)
+            {
+                string defaultName = names
+                    .Where(n => string.Equals(n, "Undefined", StringComparison.OrdinalIgnoreCase))
+                    .FirstOrDefault();
+
+                if (defaultName == null)
+                {
+                    defaultName = names.First();
+                }
+
+                return Enum.Parse(enumType, defaultName);
+            }
+
+            return null;
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            var finalValue = value.ToString();
+
+            if (!IgnoreEnumCase)
+            {
+                if (serializer.ContractResolver is CamelCasePropertyNamesContractResolver ||
+                    serializer.ContractResolver is CustomCamelCasePropertyNamesContractResolver)
+                {
+                    finalValue = finalValue.ToCamelCase();
+                }
+                else if (serializer.ContractResolver is SnakeCasePropertyNamesContractResolver)
+                {
+                    finalValue = finalValue.ToSnakeCase();
+                }
+                else if (serializer.ContractResolver is LowerCasePropertyNamesContractResolver)
+                {
+                    finalValue = finalValue.ToLowerCase();
+                }
+            }
+
+            writer.WriteValue(finalValue);
+        }
+
+        private bool IsNullableType(Type t)
+        {
+            return (t.GetTypeInfo().IsGenericType && t.GetGenericTypeDefinition() == typeof(Nullable<>));
+        }
+    }
+}
---
+++ Elasticsearch.BulkAndSearch/Helpers/JsonHelper.cs
@@ -0,0 +1,312 @@
+﻿using Elasticsearch.BulkAndSearch.Converters;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Elasticsearch.BulkAndSearch.Helpers
+{
+    public static class JsonHelper
+    {
+        private readonly static object Lock = new object();
+
+        public static List<JsonConverter> DefaultConverters = new List<JsonConverter>
+        {
+            new EnumWithContractJsonConverter(),
+            new IsoDateTimeConverter
+            {
+                DateTimeFormat = "yyyy'-'MM'-'dd'T'HH':'mm':'ss.ffffff"
+            }
+        };
+
+        public static JsonSerializerSettings SnakeCaseJsonSerializerSettings
+        {
+            get
+            {
+                if (_snakeCaseJsonSerializerSettings == null)
+                {
+                    lock (Lock)
+                    {
+                        if (_snakeCaseJsonSerializerSettings == null)
+                        {
+                            var settings = new JsonSerializerSettings();
+
+                            settings.ContractResolver = new SnakeCasePropertyNamesContractResolver();
+                            DefaultConverters.ForEach(c => settings.Converters.Add(c));
+                            settings.NullValueHandling = NullValueHandling.Ignore;
+
+                            _snakeCaseJsonSerializerSettings = settings;
+                        }
+                    }
+                }
+
+                return _snakeCaseJsonSerializerSettings;
+            }
+        }
+
+        private static JsonSerializerSettings _snakeCaseJsonSerializerSettings;
+
+        public static JsonSerializerSettings CamelCaseJsonSerializerSettings
+        {
+            get
+            {
+                if (_camelCaseJsonSerializerSettings == null)
+                {
+                    lock (Lock)
+                    {
+                        if (_camelCaseJsonSerializerSettings == null)
+                        {
+                            var settings = new JsonSerializerSettings();
+
+                            settings.ContractResolver = new CustomCamelCasePropertyNamesContractResolver();
+                            DefaultConverters.ForEach(c => settings.Converters.Add(c));
+                            settings.NullValueHandling = NullValueHandling.Ignore;
+
+                            _camelCaseJsonSerializerSettings = settings;
+                        }
+                    }
+                }
+
+                return _camelCaseJsonSerializerSettings;
+            }
+        }
+
+        public static JsonSerializerSettings _camelCaseJsonSerializerSettings;
+
+        public static JsonSerializerSettings LowerCaseJsonSerializerSettings
+        {
+            get
+            {
+                if (_lowerCaseJsonSerializerSettings == null)
+                {
+                    lock (Lock)
+                    {
+                        if (_lowerCaseJsonSerializerSettings == null)
+                        {
+                            var settings = new JsonSerializerSettings();
+
+                            settings.ContractResolver = new LowerCasePropertyNamesContractResolver();
+                            DefaultConverters.ForEach(c => settings.Converters.Add(c));
+                            settings.NullValueHandling = NullValueHandling.Ignore;
+
+                            _lowerCaseJsonSerializerSettings = settings;
+                        }
+                    }
+                }
+
+
+                return _lowerCaseJsonSerializerSettings;
+            }
+        }
+
+        private static JsonSerializerSettings _lowerCaseJsonSerializerSettings;
+
+        public static JsonSerializerSettings OriginalCaseJsonSerializerSettings
+        {
+            get
+            {
+                if (_originalCaseJsonSerializerSettings == null)
+                {
+                    lock (Lock)
+                    {
+                        if (_originalCaseJsonSerializerSettings == null)
+                        {
+                            var settings = new JsonSerializerSettings();
+
+                            settings.ContractResolver = new OriginalCasePropertyNamesContractResolver();
+                            DefaultConverters.ForEach(c => settings.Converters.Add(c));
+                            settings.NullValueHandling = NullValueHandling.Ignore;
+
+                            _originalCaseJsonSerializerSettings = settings;
+                        }
+                    }
+                }
+
+                return _originalCaseJsonSerializerSettings;
+            }
+        }
+
+        private static JsonSerializerSettings _originalCaseJsonSerializerSettings;
+
+        public static JsonSerializer CamelCaseJsonSerializer
+        {
+            get
+            {
+                if (_camelCaseJsonSerializer == null)
+                {
+                    lock (Lock)
+                    {
+
+                        if (_camelCaseJsonSerializer == null)
+                        {
+                            var serializer = new JsonSerializer();
+                            serializer.NullValueHandling = NullValueHandling.Ignore;
+                            serializer.ContractResolver = new CustomCamelCasePropertyNamesContractResolver();
+                            DefaultConverters.ForEach(c => serializer.Converters.Add(c));
+
+                            _camelCaseJsonSerializer = serializer;
+                        }
+                    }
+                }
+                return _camelCaseJsonSerializer;
+            }
+        }
+
+        private static JsonSerializer _camelCaseJsonSerializer;
+
+        public static JsonSerializer SnakeCaseJsonSerializer
+        {
+            get
+            {
+                if (_snakeCaseJsonSerializer == null)
+                {
+                    lock (Lock)
+                    {
+                        if (_snakeCaseJsonSerializer == null)
+                        {
+                            var serializer = new JsonSerializer();
+                            serializer.NullValueHandling = NullValueHandling.Ignore;
+                            serializer.ContractResolver = new SnakeCasePropertyNamesContractResolver();
+                            DefaultConverters.ForEach(c => serializer.Converters.Add(c));
+
+                            _snakeCaseJsonSerializer = serializer;
+                        }
+                    }
+                }
+
+                return _snakeCaseJsonSerializer;
+            }
+        }
+
+        private static JsonSerializer _snakeCaseJsonSerializer;
+
+        public static JsonSerializer LowerCaseJsonSerializer
+        {
+            get
+            {
+                if (_lowerCaseJsonSerializer == null)
+                {
+                    lock (Lock)
+                    {
+                        if (_lowerCaseJsonSerializer == null)
+                        {
+                            var serializer = new JsonSerializer();
+                            serializer.NullValueHandling = NullValueHandling.Ignore;
+                            serializer.ContractResolver = new LowerCasePropertyNamesContractResolver();
+                            DefaultConverters.ForEach(c => serializer.Converters.Add(c));
+                            _lowerCaseJsonSerializer = serializer;
+                        }
+                    }
+                }
+
+                return _lowerCaseJsonSerializer;
+            }
+        }
+
+        private static JsonSerializer _lowerCaseJsonSerializer;
+
+        public static JsonSerializer OriginalCaseJsonSerializer
+        {
+            get
+            {
+                if (_originalCaseJsonSerializer == null)
+                {
+                    lock (Lock)
+                    {
+                        if (_originalCaseJsonSerializer == null)
+                        {
+                            var serializer = new JsonSerializer();
+                            serializer.NullValueHandling = NullValueHandling.Ignore;
+                            serializer.ContractResolver = new OriginalCasePropertyNamesContractResolver();
+                            DefaultConverters.ForEach(c => serializer.Converters.Add(c));
+
+                            _originalCaseJsonSerializer = serializer;
+                        }
+                    }
+                }
+
+                return _originalCaseJsonSerializer;
+            }
+        }
+
+        private static JsonSerializer _originalCaseJsonSerializer;
+
+        public class LowerCaseNamingResolver : NamingStrategy
+        {
+            public LowerCaseNamingResolver()
+            {
+                this.ProcessDictionaryKeys = true;
+                this.OverrideSpecifiedNames = true;
+            }
+
+            protected override string ResolvePropertyName(string name)
+            {
+                return name.ToLowerInvariant();
+            }
+        }
+
+        public class LowerCasePropertyNamesContractResolver : DefaultContractResolver
+        {
+            public LowerCasePropertyNamesContractResolver()
+            {
+                this.NamingStrategy = new LowerCaseNamingResolver
+                {
+                    ProcessDictionaryKeys = true,
+                    OverrideSpecifiedNames = true
+                };
+            }
+        }
+
+        public class OriginalCaseNamingResolver : NamingStrategy
+        {
+            public OriginalCaseNamingResolver()
+            {
+                this.ProcessDictionaryKeys = true;
+                this.OverrideSpecifiedNames = true;
+            }
+
+            protected override string ResolvePropertyName(string name)
+            {
+                return name;
+            }
+        }
+
+        public class OriginalCasePropertyNamesContractResolver : DefaultContractResolver
+        {
+            public OriginalCasePropertyNamesContractResolver()
+            {
+                this.NamingStrategy = new OriginalCaseNamingResolver
+                {
+                    ProcessDictionaryKeys = true,
+                    OverrideSpecifiedNames = true
+                };
+            }
+        }
+
+        public class SnakeCasePropertyNamesContractResolver : DefaultContractResolver
+        {
+            public SnakeCasePropertyNamesContractResolver()
+            {
+                this.NamingStrategy = new SnakeCaseNamingStrategy
+                {
+                    ProcessDictionaryKeys = true,
+                    OverrideSpecifiedNames = true
+                };
+            }
+        }
+
+        public class CustomCamelCasePropertyNamesContractResolver : DefaultContractResolver
+        {
+            public CustomCamelCasePropertyNamesContractResolver()
+            {
+                this.NamingStrategy = new CamelCaseNamingStrategy
+                {
+                    ProcessDictionaryKeys = true,
+                    OverrideSpecifiedNames = true
+                };
+            }
+        }
+    }
+}
